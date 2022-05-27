ba0057dc945b752ec481098d5251e60f42509956
nadeemkhan02 <nadeem@billeasy.in>
2022-05-27 12:16:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function fbFun() {
+    window.open("https://www.facebook.com/login.php?skip_api_login=1&api_key=174829003346&kid_directed_site=0&app_id=174829003346&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv7.0%2Fdialog%2Foauth%3Fclient_id%3D174829003346%26state%3DAQBAG%252FLCEhzE8Kw%252FOUq8HO4sW4lqYrhyYu31%252BAGQLr%252Fi0kcKQiYzckCcRB4AZPTszZsT0Ping1l%252F9AkmFQTkRMqlK1JuREEnVHrq6CLeTeB8RjMp3NWUJEb3rWHN5a%252Fn%252BDrgPhzmdy3D4EyoFNGOhoP6b3RIchAicl4tSK7A0cJyXqaJF%252F26x1lk2KeyI8o2Sgmv%252BZJH9evrEWdiNjbhBqBEoZ19pmGGuN4L5qs%253D%26redirect_uri%3Dhttps%253A%252F%252Faccounts.spotify.com%252Flogin%252Ffacebook%252Fredirect%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dd0a21946-b4dc-4a5f-b6a9-742f2a1c210c%26tp%3Dunspecified&cancel_url=https%3A%2F%2Faccounts.spotify.com%2Flogin%2Ffacebook%2Fredirect%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3DAQBAG%252FLCEhzE8Kw%252FOUq8HO4sW4lqYrhyYu31%252BAGQLr%252Fi0kcKQiYzckCcRB4AZPTszZsT0Ping1l%252F9AkmFQTkRMqlK1JuREEnVHrq6CLeTeB8RjMp3NWUJEb3rWHN5a%252Fn%252BDrgPhzmdy3D4EyoFNGOhoP6b3RIchAicl4tSK7A0cJyXqaJF%252F26x1lk2KeyI8o2Sgmv%252BZJH9evrEWdiNjbhBqBEoZ19pmGGuN4L5qs%253D%23_%3D_&display=page&locale=en_GB&pl_dbl=0")
+}
+function appleFun() {
+    window.open("https://appleid.apple.com/auth/authorize?client_id=com.spotify.accounts&response_type=code&response_mode=form_post&scope=name%20email&state=AQC7hWIzMGtFpIk4fuCJsLTa5BuxMqj8qwnPBXkgWM72LVAa4JUEjfUlYFdjTdUZNrdx4KaJTp24OAfgy%2BAnUHXxbCxvatTWAAlelJoGtUuGiM1drvRCCxhwCvB6dDOaQqW8vwAINbbA59Ya8YfOIeknNu3P4afFFob574PGLsAB9VD63oMActNaVTPf%2FD%2F6SiltQRacBw8DetmzYatlo%2BA%2Bqct0tdFg7R1ygaM%3D&redirect_uri=https%3A%2F%2Faccounts.spotify.com%2Flogin%2Fapple%2Fredirect")
+}
+function googleFun() {
+    window.open("https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&access_type=offline&client_id=1046568431490-ij1gi5shcp2gtorls09frkc56d4mjbe2.apps.googleusercontent.com&state=AQAB5kjbzCopp%2FEZicMVbRt8grX1qdBOrBMc8Q28brmLKaE7QTHrTguzgl9y8HzNfcBGsnSsCfYxUPc8HtfnLp8TwgfhDPkadWcnUcGuM%2FGv0wuggUjqjvKCnothrjNX6vkMmhSAPkVY6RmuvVqVT8Rmm8sLwI4acLuA38FX0AWxJHE6wm%2BF%2BcQ4Wg5i4gRikNsRUjyS3ueN6yAUmSLDE8oL9odKd4qrmm51FxI%3D&scope=profile%20email%20openid&redirect_uri=https%3A%2F%2Faccounts.spotify.com%2Flogin%2Fgoogle%2Fredirect&flowName=GeneralOAuthFlow")
+}
+function phoneFun() {
+    window.open("https://accounts.spotify.com/en/login/phone?continue=https%3A%2F%2Fwww.spotify.com%2Faccount%2Foverview%2F")
+}
+function signUp() {
+    window.location.href = "signup.html"
+}
